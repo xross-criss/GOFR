32b23787b1f8d61cdd6a4eebc127dd96b3d71e45
Krzysiek <krzysztof.kwiatkowski@morecraft.pl>
2016-10-27 15:15:48 +0200

    Error occures on Cells becoming randomly DEAD or ALIVE inside Steps - no3 Input to check, corrected printing.

--- src/main/java/xyz/morecraft/dev/xross/gofr/io/Output.java
+++ src/main/java/xyz/morecraft/dev/xross/gofr/io/Output.java
@@ -1,10 +1,16 @@
 package xyz.morecraft.dev.xross.gofr.io;
 
+import xyz.morecraft.dev.xross.gofr.engine.CellState;
 import xyz.morecraft.dev.xross.gofr.world.World;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Output {
+
     public static World readFileFromTXT(String filename) {
-        //TODO - readFileFromTXT
         return null;
     }
 }
